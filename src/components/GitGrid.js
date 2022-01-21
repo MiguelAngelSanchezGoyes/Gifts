68a13619ec066f68fe893f86827f1e1eb47469bb
MiguelAngel <miguel.sanchez.goyes@hotmail.com>
2022-01-21 10:35:33 +0100

    css + helper

--- src/components/GitGrid.js
+++ src/components/GitGrid.js
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getGifs } from "../helpers/getGifs";
+import { GifGridItem } from "./GifGridItem";
 
 export const GitGrid = ({category})=>{
 
-    const getGifs =async()=>{
-        const url ='https://api.giphy.com/v1/gifs/search?q=Rick and Morty&limit=10&api_key=t22RKYS2UQd8mjThrSOpsPiVE5miXdMZ'
-        const resp = await fetch(url);
-        const {data} = await resp.json();
+    const [images, setImages]= useState([]);
 
-        const gifts = data.map(img=>{
-            return{
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-        console.log(gifts)
-    }
+    useEffect(()=>{
+        getGifs(category).then(imgs=>setImages(imgs))
+    }, [category])
 
-    getGifs();
+   
 
 
     return(
-        <div>
-            <h3>{category}</h3>
+        <>
+        <h3>{category}</h3>
+        <div className="card-grid">
+            
+            
+                {
+                    images.map(img=>(
+                        <GifGridItem key={img.id} {...img}/>
+                    ))
+                }
+            
 
         </div>
+        </>
+        
     )
 }
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs =async(category)=>{
+    const url =`https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=t22RKYS2UQd8mjThrSOpsPiVE5miXdMZ`
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+
+    const gifts = data.map(img=>{
+        return{
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+    return gifts
+}
